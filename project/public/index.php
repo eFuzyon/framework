7bd8f58fcad92d2ecf7bc38fe4f6b4c22730e930
Leandro Sanches <leandro.sanches@vagast.com.br>
2016-04-05 23:12:46 -0300

    Fixes

---
+++ project/public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+	/**
+	 * eFuzyon Framework
+	 *
+	 * @package  eFuzyon\Framework
+	 * @author   Leandro Sanches <leandro.sanches@efuzyon.com>
+	 */
+
+	/*
+	|---------------------------------------------------------
+	| Auto Loader
+	|---------------------------------------------------------
+	*/
+	require __DIR__ . "/../vendor/autoload.php";
+
+	ini_set("display_errors", "off");
+	$de = ini_get("display_errors");
+
+	use Symfony\Component\Debug\Debug;
+	use Symfony\Component\Debug\ExceptionHandler;
+	use Symfony\Component\Debug\ErrorHandler;
+
+	#DebugClassLoader::enable();
+	
+	if ($de == "on") :
+		Debug::enable();
+	else:
+		ErrorHandler::register()->setExceptionHandler(function($e){
+			include __DIR__ . "/../views/error/500.tpl";
+		});
+	endif;
+	
+	include 123;
