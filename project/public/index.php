e9e33cff3404fc1e2b0895bb23de3d92a3f242fb
Leandro Sanches <leandro.sanches@vagast.com.br>
2016-04-05 21:32:03 -0300

    First commit - Initial Setup

---
+++ project/public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+	/**
+	 * eFuzyon Framework
+	 *
+	 * @package  eFuzyon\Framework
+	 * @author   Leandro Sanches <leandro.sanches@efuzyon.com>
+	 */
+
+	/*
+	|--------------------------------------------------------------------------
+	| Register The Auto Loader
+	|--------------------------------------------------------------------------
+	*/
+
+	require __DIR__ . "/../vendor/autoload.php";
