1d86cab5f14cffabc1e52e25096f2e6b4a09b568
Leandro Sanches <leandro.sanches@vagast.com.br>
2016-04-07 23:29:54 -0300

    Composer changes, new classes on core and routes

--- core/classes/route.class.php
+++ core/classes/route.class.php
@@ -2,14 +2,20 @@
 
 	namespace Core;
 
+	use Symfony\Component\HttpFoundation\Request;
+
 	class Route 
 	{
 
 		public static function Init(){
 
-			Debug::Call('generic', [
-				'error-message' => 'Waiting for Routes'
-			]);
+			#$request = Request::createFromGlobals();
+
+			/**
+			* Parse: app/config/routes.yml
+			*/
+			$router = Yaml::LoadRouter(BASE_PATH_APP . 'config/routes.yml');
+			if ($router) $match = $router->match('/');
 
 		}
 
